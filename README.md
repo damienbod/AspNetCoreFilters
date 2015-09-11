4179dab222c94876b1d253f3844de7ebc4bce1bd
damienbod <damien_bod@hotmail.com>
2015-09-11 10:48:02 +0200

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# AspNet5Filters
---
+++ src/AspNet5Filters/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.DependencyInjection;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
